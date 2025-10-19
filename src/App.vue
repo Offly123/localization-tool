5e20b046a19f9a74fbbcb5a94851251d99c8f249
Offly123 <kaysinegor@mail.com>
2025-10-19 22:01:40 +0300

    created repo

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Localization-tool</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+div {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
